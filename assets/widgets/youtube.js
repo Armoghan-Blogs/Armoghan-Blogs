9669e23a3ab14ae04f1e423ebd657242e3d2a9f2
Armoghan <Armoghan16@gmail.com>
2024-08-09 12:20:12 +0500

    Update youtube.js

--- assets/widgets/youtube.js
+++ assets/widgets/youtube.js
@@ -17,23 +17,34 @@ CMS.registerEditorComponent({
       widget: 'string',
       default: '',
       hint: 'Enter a label or description for the video. This is optional.'
+    },
+    {
+      name: 'params',
+      label: 'Parameters',
+      widget: 'string',
+      required: false,
+      default: '',
+      hint: 'Enter additional parameters for the video URL, e.g., start=130&end=10&controls=0. Separate multiple parameters with "&".'
     }
   ],
-  pattern: /^{{< youtubeLite id="([^"]+)"(?: label="([^"]*)")? >}}$/,
+  pattern: /^{{< youtubeLite id="([^"]+)"(?: label="([^"]*)")?(?: params="([^"]*)")? >}}$/,
   fromBlock: function (match) {
     return {
       id: match[1],
-      label: match[2] || ''
+      label: match[2] || '',
+      params: match[3] || ''
     };
   },
   toBlock: function (data) {
-    return `{{< youtubeLite id="${data.id}"${data.label ? ` label="${data.label}"` : ''} >}}`;
+    return `{{< youtubeLite id="${data.id}"${data.label ? ` label="${data.label}"` : ''}${data.params ? ` params="${data.params}"` : ''} >}}`;
   },
   toPreview: function (data) {
+    const { id, label, params } = data;
+    const url = `https://www.youtube.com/embed/${id}${params ? `?${params}` : ''}`;
     return `
       <div>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/${data.id}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        ${data.label ? `<p>${data.label}</p>` : ''}
+        <iframe width="560" height="315" src="${url}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        ${label ? `<p>${label}</p>` : ''}
       </div>
     `;
   }
