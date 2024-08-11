398bcfcb3013a53a9a8601ea356626dc06180d7e
Armoghan <Armoghan16@gmail.com>
2024-08-12 02:58:12 +0500

    chore(lint): add ESLint configuration and setup

---
+++ eslint.config.cjs
@@ -0,0 +1,53 @@
+/** @type {import('eslint').Linter.FlatConfig} */
+module.exports = [
+  {
+    ignores: [
+      '**/node_modules/**',
+      '**/.github/**',
+      '**/.vscode/**',
+      '**/.git/**',
+      '**/.devcontainer/**',
+      '**/.husky/**',
+      '**/resources/**',
+      '**/themes/**',
+      '**/static/**',
+      '**/package.json',
+      '**/package-lock.json',
+      '**/.prettierrc.js',
+      '**/.eslintignore',
+    ]
+  },
+  {
+    files: ["**/*.js"],
+    linterOptions: {
+      noInlineConfig: true,
+    },
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+      globals: {
+        window: 'readonly',
+        document: 'readonly',
+        fetch: 'readonly',
+      },
+    },
+    plugins: {
+      prettier: require('eslint-plugin-prettier'), // Include the Prettier plugin
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      'semi': ['error', 'always'],
+      'quotes': ['error', 'double'],
+      'comma-dangle': ['error', 'always-multiline'],
+      'indent': ['error', 2],
+      'lines-around-comment': ['error', { beforeBlockComment: true }],
+      'no-unused-vars': ['error', { vars: 'all', args: 'none', ignoreRestSiblings: false }],
+      'no-extra-semi': 'error',
+      'no-console': 'warn',
+      'object-curly-spacing': ['error', 'always'],
+      'space-in-parens': ['error', 'never'],
+      'camelcase': ['error', { properties: 'always' }],
+      'function-call-argument-newline': ['error', 'consistent']
+    }
+  }
+];
